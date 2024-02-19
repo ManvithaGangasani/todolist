149626ddba8188e12c7f0c54e443c47fcc406c85
Manvitha Gangasani <manvithareddy0210>
2024-02-19 09:10:13 +0530

    done

--- 11-todo-list.js
+++ 11-todo-list.js
@@ -18,15 +18,21 @@ for(let i=0;i<todoList.length;i++){
             <div>${dueDate}</div>
             <button onclick="todoList.splice(${i},1); renderTodoList();" class="delete-todo-button">Delete</button>`;
             todoListHtml+=html;
+
 }
 console.log(todoListHtml);
 document.querySelector('.js-todo-list').innerHTML=todoListHtml;
 }
+
 function addTodo(){
    const inputElement= document.querySelector('.js-name-input');
    const name=inputElement.value;
    const dateInputElement= document.querySelector('.js-due-date-input');
    const dueDate=dateInputElement.value;
+   if (name === '' || dueDate === '') {
+      alert('Please enter both name and due date for the to-do item.');
+      return;
+    }
    todoList.push({ name,dueDate});
 
    inputElement.value='';
